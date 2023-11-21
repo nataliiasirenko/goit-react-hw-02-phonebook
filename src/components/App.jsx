22187275528417e52e317ea04e4e2e9bf04fc804
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-11-21 20:30:51 +0000

    update phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import React from 'react';
+
+import { nanoid } from 'nanoid';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <>React homework template</>;
 };
